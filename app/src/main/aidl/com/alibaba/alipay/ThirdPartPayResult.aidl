4fc06470f00edbc066fd0350d45f1f8e025143ff
ak <1450232082@qq.com>
2022-06-08 17:40:16 +0800

    default commit

--- app/src/main/aidl/com/alibaba/alipay/ThirdPartPayResult.aidl
+++ app/src/main/aidl/com/alibaba/alipay/ThirdPartPayResult.aidl
@@ -2,8 +2,14 @@ package com.alibaba.alipay;
 
 interface ThirdPartPayResult {
 
+    /**
+     * 支付成功
+     */
     void onPaySuccess(float money);
 
+    /**
+     * 支付失败
+     */
     void onPayFailed(in int errorCode,String msg);
 
 }
--- app/src/main/java/com/alibaba/alipay/PayActivity.java
+++ app/src/main/java/com/alibaba/alipay/PayActivity.java
@@ -8,6 +8,10 @@ import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -23,10 +27,10 @@ public class PayActivity extends AppCompatActivity {
 
     private TextView orderInfoTv;
     private TextView payMoneyTv;
-    private EditText passwordInput;
     private Button payCommitBtn;
 
     private PayService.PayAction payAction;
+    private Dialog dialog;
     private Dialog loading;
 
 
@@ -80,30 +84,55 @@ public class PayActivity extends AppCompatActivity {
     private void initView() {
         orderInfoTv = (TextView) findViewById(R.id.order_info_tv);
         payMoneyTv = (TextView) findViewById(R.id.pay_money_tv);
-        passwordInput = (EditText) findViewById(R.id.password_input);
         payCommitBtn = (Button) findViewById(R.id.pay_commit_btn);
-        loading = new MaterialAlertDialogBuilder(this).setView(new ProgressBar(this)).create();
 
         Intent intent = getIntent();
         String payInfo = intent.getStringExtra(Constants.KEY_BILL_INFO);
         float payMoney = intent.getFloatExtra(Constants.KEY_PAY_MONEY, 0);
         orderInfoTv.setText("支付信息：" + payInfo);
         payMoneyTv.setText("金额：" + payMoney + "元");
+
+        loading = new Dialog(this);
+        loading.setContentView(R.layout.dialog_loading);
+        loading.setCanceledOnTouchOutside(false);
+        loading.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+        loading.create();
+
+        View pwdView = getLayoutInflater().inflate(R.layout.dialog_pwd, null, false);
+        EditText pwdEdt = pwdView.findViewById(R.id.pwd_edt);
+        pwdEdt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.length() != 6) return;
+                if (Constants.KEY_PASSWORD.equals(s.toString()) && payAction != null) {
+                    dialog.dismiss();
+                    loading.show();
+
+                    new Handler().postDelayed(() -> {
+                        payAction.pay(payMoney);
+                        loading.dismiss();
+                        finish();
+                    }, 1000);
+                } else {
+                    Toast.makeText(PayActivity.this, "密码错误", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        dialog = new MaterialAlertDialogBuilder(this).setView(pwdView).create();
+
         //确认支付
         payCommitBtn.setOnClickListener(v -> {
-            String pwd = passwordInput.getText().toString().trim();
-            if (Constants.KEY_PASSWORD.equals(pwd) && payAction != null) {
-                loading.show();
-                new Handler().postDelayed(() -> {
-                    if (loading.isShowing()) {
-                        loading.dismiss();
-                    }
-                    payAction.pay(payMoney);
-                    finish();
-                }, 1000);
-            } else {
-                Toast.makeText(this, "密码错误", Toast.LENGTH_SHORT).show();
-            }
+            dialog.show();
         });
     }
 
